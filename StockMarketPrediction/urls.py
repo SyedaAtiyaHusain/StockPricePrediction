0e6f05e0e332e4538af0a8d40f0a40ec4dffb473
SyedaAtiyaHusain <53337550+SyedaAtiyaHusain@users.noreply.github.com>
2020-04-25 05:15:25 +0530

    Second Commit

--- StockMarketPrediction/urls.py
+++ StockMarketPrediction/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+#Defining the urls 
+
 urlpatterns = [
     path('',views.index,name='stockhome'),
     path('features/',views.features,name='features'),
